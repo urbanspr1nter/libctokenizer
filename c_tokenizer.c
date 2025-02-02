a511800089d543d7cf938ee8b50dfedadffd477b
Roger Ngo <urbanspr1nter@gmail.com>
2025-02-01 20:45:04 -0800

    Basic operations

--- c_tokenizer.c
+++ c_tokenizer.c
@@ -21,11 +21,52 @@ static void safeFree(void* ptr) {
     free(ptr);
 }
 
-void ctok_free(struct TokenCollection* collection) {
-    for (size_t i = 0; i < collection->count; i++) {
-        cstring_free(collection->tokens[i]->value);
-        safeFree(collection->tokens[i]);
+static struct Token* createToken(String* text) {
+    struct Token* token = safeMalloc(sizeof(struct Token));
+
+    token->value = text;
+
+    return token;
+}
+
+static void push(struct TokenCollection* collection, struct Token* token) {
+    float lf = (float)collection->count / collection->length;
+    if (lf > 0.75) {
+        uint64_t newLength = collection->length * 2;
+
+        struct Token** newTokens = safeMalloc(newLength * sizeof(struct Token*));
+        for (size_t i = 0; i < collection->count; i++) {
+            String* newTokenText;
+            newTokenText = cstring_create(newTokenText, collection->tokens[i]->value->text);
+            newTokens[i] = createToken(newTokenText);
+        }
+
+        freeTokens(collection->tokens, collection->count);
+        safeFree(collection->tokens);
+
+        collection->tokens = newTokens; 
+        collection->length = newLength;
     }
+
+    collection->tokens[collection->count] = token;
+    collection->count++;
+}
+
+static void freeTokens(struct Token** tokens, uint64_t count) {
+    for (size_t i = 0; i < count; i++) {
+        cstring_free(tokens[i]->value);
+        safeFree(tokens[i]);
+    }
+}
+
+void ctok_free(struct TokenCollection* collection) {
+    freeTokens(collection->tokens, collection->count);
     safeFree(collection->tokens);
     safeFree(collection);
+}
+
+void ctok_printTokens(struct TokenCollection* collection) {
+    for (size_t i = 0; i < collection->count; i++) {
+        printf("%s\n", collection->tokens[i]->value->text);
+    }
 }
