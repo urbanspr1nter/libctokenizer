fbd2810e63e6e3a392527cb65988b0b6deb14cef
Roger Ngo <urbanspr1nter@gmail.com>
2025-02-01 22:25:11 -0800

    c_tokenizer.c

--- c_tokenizer.c
+++ c_tokenizer.c
@@ -2,10 +2,14 @@
 #include <stdlib.h>
 #include "c_tokenizer.h"
 
+#ifndef MAX_WORD_BUFFER_LENGTH
+#define MAX_WORD_BUFFER_LENGTH 512
+#endif
+
 static void* safeMalloc(size_t size) {
     void* ptr = malloc(size);
     if (!ptr) {
-        fprintf(stderr, "safeMalloc - couldn't allocate pointer of %d bytes.\n", size);
+        fprintf(stderr, "safeMalloc - couldn't allocate pointer of %ld bytes.\n", size);
         exit(1);
     }
 
@@ -27,6 +31,13 @@ static struct Token* createToken(String* text) {
     token->value = text;
 
     return token;
+}
+
+static void freeTokens(struct Token** tokens, uint64_t count) {
+    for (size_t i = 0; i < count; i++) {
+        cstring_free(tokens[i]->value);
+        safeFree(tokens[i]);
+    }
 }
 
 static void push(struct TokenCollection* collection, struct Token* token) {
@@ -52,12 +63,59 @@ static void push(struct TokenCollection* collection, struct Token* token) {
     collection->count++;
 }
 
-static void freeTokens(struct Token** tokens, uint64_t count) {
-    for (size_t i = 0; i < count; i++) {
-        cstring_free(tokens[i]->value);
-        safeFree(tokens[i]);
+struct TokenCollection* ctok_tokenize(String* data, String* delimeter) {
+    uint64_t initialTokensCollectionLength = 32;
+
+    uint8_t delimeterTable[256] = {0};
+    for (size_t i = 0; i < delimeter->length; i++) {
+        delimeterTable[(uint8_t)delimeter->text[i]] = 1;
     }
+
+    struct TokenCollection* collection = (struct TokenCollection*)safeMalloc(sizeof(struct TokenCollection));
+    collection->tokens = (struct Token**)safeMalloc(sizeof(struct Token*) * initialTokensCollectionLength);
+    collection->count = 0;
+    collection->length = initialTokensCollectionLength;
+
+    char* wordBuffer = (char*)safeMalloc(sizeof(char) * MAX_WORD_BUFFER_LENGTH);
+    size_t wordBufferIdx = 0;
+
+    for (size_t i = 0; i < data->length; i++) {
+        char c = data->text[i];
+        if (delimeterTable[(uint8_t)c]) {
+            if (wordBufferIdx > 0) {
+                String* wordStr;
+                wordStr = cstring_create(wordStr, wordBuffer);
+
+                push(collection, createToken(wordStr));
+            }
+
+            // add the delimeter
+            String* delimStr;
+            delimStr = cstring_charToString(delimStr, c);
+            push(collection, createToken(delimStr));
+
+            // reset 
+            wordBufferIdx = 0;
+            wordBuffer[0] = '\0';
+        } else if (i + 1 == data->length) {
+            String* wordStr;
+            wordStr = cstring_create(wordStr, wordBuffer);
+
+            push(collection, createToken(wordStr));
+
+            wordBufferIdx = 0;
+            wordBuffer[0] = '\0';
+        } else {
+            wordBuffer[wordBufferIdx] = c;
+            wordBufferIdx++;
+            wordBuffer[wordBufferIdx] = '\0';
+        }
+    }
+
+    safeFree(wordBuffer);
+    return collection;
 }
+
 
 void ctok_free(struct TokenCollection* collection) {
     freeTokens(collection->tokens, collection->count);
